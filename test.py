65f42e9decf8cbe1d3d59d02de32e4ed49b7ae53
discus7l <133470486+discus7l@users.noreply.github.com>
2023-05-14 19:45:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,28 @@
-######------------ Original Script ------------######
+######----------------- Original Script ----------------######
 # import sheetControl
-
 # response = sheetControl.myFunction()
 # print(response)
 
-######------------ In folder ------------######
+######------------------- In folder --------------------######
 # from locPack import sheetControl
-
 # response = sheetControl.myFunction()
 # print(response)
 
 ######------------ In folder /w __init__.py ------------######
 # With init.py we can call the function within the module within the package in short calls
 # virtually skipping the module name
-# "package.func" rather than "package.module.function"
-# from locPack.sheetControl import myFunction as func
-import locPack
+# "package.function" rather than "package.module.function"
+# We can also keep the import statement short, if that matters
+# "import locPack" rather than "from locPack inport sheetControl"
+# REF https://stackoverflow.com/questions/1944569/how-do-i-write-good-correct-package-init-py-files
 
+# __init__.py content
+# from locPack.sheetControl import myFunction as func
+
+# Debian keeps site-packages in a strange place
+# Below is the location in Debian
+# /usr/local/lib/python3.9/dist-packages
+
+import locPack
 response = locPack.func()
 print(response)
